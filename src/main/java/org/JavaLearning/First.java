27e800227906e8bf98c11525f325976859d79eeb
Gaurav-023 <gauravsinghania812@gmail.com>
2023-09-12 18:23:17 +0530

    Java Practices Code

---
+++ src/main/java/org/JavaLearning/First.java
@@ -0,0 +1,15 @@
+package org.JavaLearning;
+
+public class First
+{
+    public static void main(String[] args)
+    {
+        int a = 1;
+        System.out.println("Hello ");
+        for (int i = 0; i < 5; i++)
+        {
+            a+=1;
+            System.out.println(""+a);
+        }
+    }
+}
